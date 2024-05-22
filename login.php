4ba47f754ee8ba32f996141a27ee874e995a9a94
Solo Coder <148307188+ShanMihirangaHerath@users.noreply.github.com>
2024-05-22 22:14:01 +0530

    Initial commit

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register | Click-Cart</title>
+    <!-- MATERIAL DESIGN ICONIC FONT -->
+    <link rel="stylesheet" href="resource\font\material-design-iconic-font\css\material-design-iconic-font.min.css">
+    <!--Stylesheets Links-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="wrapper" style="background-image: url('resource/image/bg-registration-form-2.jpg');">
+        <div class="inner">
+            <form action="">
+                <h3>Login</h3>
+                <div class="form-wrapper">
+                    <label for="">Email</label>
+                    <input type="email" class="form-control" id="" placeholder="">
+                </div>
+                <div class="form-wrapper">
+                    <label for="">Password</label>
+                    <input type="password" class="form-control" id="" placeholder="">
+                </div>
+                <div class="form-wrapper">
+                    <label>
+                        <input type="checkbox">Remember me.
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <button>Login</button>
+                    <button><a href="register.php">Register</a></button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register | Click-Cart</title>
+    <!-- MATERIAL DESIGN ICONIC FONT -->
+    <link rel="stylesheet" href="resource\font\material-design-iconic-font\css\material-design-iconic-font.min.css">
+    <!--Stylesheets Links-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="wrapper" style="background-image: url('resource/image/bg-registration-form-2.jpg');">
+        <div class="inner">
+            <form action="">
+                <h3>Register</h3>
+                <div class="form-group">
+                    <div class="form-wrapper">
+                        <label for="">First Name</label>
+                        <input type="text" class="form-control" id="" placeholder="">
+                    </div>
+                    <div class="form-wrapper">
+                        <label for="">Last Name</label>
+                        <input type="text" class="form-control" id="" placeholder="">
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <label for="">Email</label>
+                    <input type="email" class="form-control" id="" placeholder="">
+                </div>
+                <div class="form-wrapper">
+                    <label for="">Password</label>
+                    <input type="password" class="form-control" id="" placeholder="">
+                </div>
+                <div class="form-wrapper">
+                    <label for="">Confirm Password</label>
+                    <input type="password" class="form-control" id="" placeholder="">
+                </div>
+                <div class="form-wrapper">
+                    <label>
+                        <input type="checkbox">I accept the terms and conditions.
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <button>Register Now</button>
+                    <button><a href="login.php">Login</a></button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
